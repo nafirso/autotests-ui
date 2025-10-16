63bc91fbcb5a0d0446afe3fd2080c9caf3d79290
nikolay <firnikant87@gmail.com>
2025-10-16 13:03:15 +0300

    new files

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -11,6 +11,7 @@ class RegistrationPage(BasePage):
         self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
         self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
         self.registration_button = page.get_by_test_id('registration-page-registration-button')
+        self.login_link = page.get_by_test_id('registration-page-login-link')
 
     def fill_registration_form(self, email: str, username: str, password: str):
         self.email_input.fill(email)
@@ -24,3 +25,6 @@ class RegistrationPage(BasePage):
 
     def click_registration_button(self):
         self.registration_button.click()
+
+    def click_login_link(self):
+        self.login_link.click()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,20 +1,18 @@
 import pytest
-from playwright.sync_api import Page
+
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-@pytest.mark.parametrize(
-    "email, username, password",
-    [
-        ("user.name@gmail.com", "username", "password"),
-    ])
-def test_successful_registration(chromium_page: Page, email: str, username: str, password: str):
-    registration_page = RegistrationPage(page=chromium_page)
+def test_successful_registration(dashboard_page: DashboardPage, registration_page: RegistrationPage):
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(email=email, username=username, password=password)
+    registration_page.fill_registration_form(
+        email="user.name@gmail.com",
+        username="username",
+        password="password"
+    )
     registration_page.click_registration_button()
-    dashboard_page = DashboardPage(page=chromium_page)
-    dashboard_page.check_visible_dashboard_toolbar_title()
+
+    dashboard_page.check_visible_dashboard_title()
